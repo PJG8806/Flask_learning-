d8988a2a338e80e4ba953536543831acebee84bc
PJG8806 <jinguy39@gmail.com>
2025-12-23 09:53:37 +0900

    주석 추가

--- Day2/app.py
+++ Day2/app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 app.config["API_TITLE"] = "My API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.1.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+# 위 항목은 필수 설정
+app.config["OPENAPI_URL_PREFIX"] = "/" # 기본 경로 설정
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui" # http://127.0.0.1:5000/swagger-ui 접속시 swagger 문서를 웹으로 보는 주소 설정
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/" # swagger ui의 js/css 파일을 어디서 가져올지 지정하는 설정
 
 api = Api(app)
 api.register_blueprint(blp)
